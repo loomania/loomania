b25d51525ec150fb6b53d40688055b7e9a5e5e81
David M. Lloyd <david.lloyd@redhat.com>
2022-09-23 11:25:25 -0500

    Initial

---
+++ apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"io.github.dmlloyd.loomania","l":"Loomania"},{"p":"io.github.dmlloyd.loomania","l":"VirtualThreadManager"}];updateSearchResults();
