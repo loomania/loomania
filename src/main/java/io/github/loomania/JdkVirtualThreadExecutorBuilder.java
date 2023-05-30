bf1a99091bac821927036574b4792376e77584bf
David M. Lloyd <david.lloyd@redhat.com>
2023-05-30 11:45:14 -0500

    Finish up JavaDoc

--- src/main/java/io/github/loomania/JdkVirtualThreadExecutorBuilder.java
+++ src/main/java/io/github/loomania/JdkVirtualThreadExecutorBuilder.java
@@ -20,62 +20,133 @@ public final class JdkVirtualThreadExecutorBuilder {
 
     JdkVirtualThreadExecutorBuilder() {}
 
+    /**
+     * Get the parallelism value set on this builder.
+     *
+     * @return the parallelism value
+     */
     public int getParallelism() {
         return parallelism;
     }
 
+    /**
+     * Set the parallelism value.
+     *
+     * @param parallelism the parallelism value
+     * @return this builder (not {@code null})
+     */
     public JdkVirtualThreadExecutorBuilder setParallelism(final int parallelism) {
         this.parallelism = parallelism;
         return this;
     }
 
+    /**
+     * Get the core pool size.
+     *
+     * @return the core pool size
+     */
     public int getCorePoolSize() {
         return corePoolSize;
     }
 
+    /**
+     * Set the core pool size.
+     *
+     * @param corePoolSize the core pool size
+     * @return this builder (not {@code null})
+     */
     public JdkVirtualThreadExecutorBuilder setCorePoolSize(final int corePoolSize) {
         this.corePoolSize = corePoolSize;
         return this;
     }
 
+    /**
+     * Get the maximum pool size.
+     *
+     * @return the maximum pool size
+     */
     public int getMaximumPoolSize() {
         return maximumPoolSize;
     }
 
+    /**
+     * Set the maximum pool size.
+     *
+     * @param maximumPoolSize the maximum pool size
+     * @return this builder (not {@code null})
+     */
     public JdkVirtualThreadExecutorBuilder setMaximumPoolSize(final int maximumPoolSize) {
         this.maximumPoolSize = maximumPoolSize;
         return this;
     }
 
+    /**
+     * Get the minimum runnable count.
+     *
+     * @return the minimum runnable count
+     */
     public int getMinimumRunnable() {
         return minimumRunnable;
     }
 
+    /**
+     * Set the minimum runnable count.
+     *
+     * @param minimumRunnable the minimum runnable count
+     * @return this builder (not {@code null})
+     */
     public JdkVirtualThreadExecutorBuilder setMinimumRunnable(final int minimumRunnable) {
         this.minimumRunnable = minimumRunnable;
         return this;
     }
 
+    /**
+     * Get the keep-alive time.
+     *
+     * @return the keep-alive time
+     */
     public Duration getKeepAliveTime() {
         return keepAliveTime;
     }
 
+    /**
+     * Set the keep-alive time.
+     *
+     * @param keepAliveTime the keep-alive time (must not be {@code null})
+     * @return this builder (not {@code null})
+     */
     public JdkVirtualThreadExecutorBuilder setKeepAliveTime(final Duration keepAliveTime) {
         Objects.requireNonNull(keepAliveTime);
         this.keepAliveTime = keepAliveTime;
         return this;
     }
 
+    /**
+     * Get the thread pool name.
+     *
+     * @return the thread pool name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Set the thread pool name.
+     *
+     * @param name the thread pool name (must not be {@code null})
+     * @return this builder (not {@code null})
+     */
     public JdkVirtualThreadExecutorBuilder setName(final String name) {
         Objects.requireNonNull(name);
         this.name = name;
         return this;
     }
 
+    /**
+     * Build a new executor service with this builder's current configuration.
+     *
+     * @return the executor service (not {@code null})
+     */
     public ExecutorService build() {
         return Loomania.buildVirtualThreadFactory(this);
     }
--- src/main/java/io/github/loomania/Loomania.java
+++ src/main/java/io/github/loomania/Loomania.java
@@ -18,14 +18,36 @@ public final class Loomania {
         return false;
     }
 
+    /**
+     * Create a single-threaded virtual thread executor service which uses an event loop to schedule tasks.
+     *
+     * @param carrierThreadFactory the thread factory for carrier threads (must not be {@code null})
+     * @param eventLoop the event loop implementation (must not be {@code null})
+     * @param listener the executor service listener (must not be {@code null}, may be {@link ExecutorServiceListener#EMPTY})
+     * @return the new executor service (not {@code null})
+     */
     public static ExecutorService newEventLoopExecutorService(ThreadFactory carrierThreadFactory, EventLoop eventLoop, ExecutorServiceListener listener) {
         throw Nope.nope();
     }
 
+    /**
+     * Create a virtual thread executor which uses the given delegate executor service to schedule tasks.
+     * Note that the executor service must be "well-behaved" in order to prevent situations such as deadlocks.
+     *
+     * @param delegate the delegate executor service (must not be {@code null})
+     * @param name the name of the new executor (must not be {@code null})
+     * @param listener the executor service listener (must not be {@code null}, may be {@link ExecutorServiceListener#EMPTY})
+     * @return the new executor service (not {@code null})
+     */
     public static ExecutorService newVirtualThreadExecutor(ExecutorService delegate, String name, ExecutorServiceListener listener) {
         throw Nope.nope();
     }
 
+    /**
+     * Create a builder for a fork-join-pool-based virtual thread executor.
+     *
+     * @return the new builder (not {@code null})
+     */
     public static JdkVirtualThreadExecutorBuilder newJdkVirtualThreadExecutorBuilder() {
         throw Nope.nope();
     }
